ebe2e70178d0e3ca5d9edbfe2f989e69cba3cbe1
Hildebrando Rueda <harq.soft@gmail.com>
2015-12-21 17:13:04 -0500

    Add pony gem

--- app.rb
+++ app.rb
@@ -10,6 +10,19 @@ require './models/price'
 
 configure do
   Mongoid.load!('./database.yml', ENV['RACK_ENV'].to_sym)
+
+  Pony.options = {
+    :via => :smtp,
+    :via_options => {
+      :address => 'smtp.sendgrid.net',
+      :port => '587',
+      :domain => 'heroku.com',
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
+  }
 end
 
 get '/' do
